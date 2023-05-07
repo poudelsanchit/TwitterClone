22aac53c8f656808763dd543e862a91f4a7b5483
Sanchit Poudel <snchtpdl12@gmail.com>
2023-05-07 18:25:27 +0545

    Problem solved

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -3,18 +3,22 @@ import TweetBody from "./tweetbody";
 import Footer from "./footer";
 
 const Tweet = ({ name, username, time, tweet, image, avatar, id }) => {
-    return (<div className="tweet">
-        <img
-            className="profile-pic"
-            src={avatar}
-            alt="Profile Picture"
-            height={100}
-        />
-        <div className="tweet-content">
-            <Header name={name} username={username} time={time} />
-            <TweetBody tweet={tweet} image={image} id={id} />
-            <Footer />
-        </div>
-    </div >);
+    return (
+        <>
+            <div className="tweet">
+                <img
+                    className="profile-pic"
+                    src={avatar}
+                    alt="Profile Picture"
+                    height={100}
+                />
+                <div className="tweet-content">
+                    <Header name={name} username={username} time={time} />
+                    <TweetBody tweet={tweet} image={image} id={id} />
+                    <Footer />
+                </div>
+            </div >
+        </>
+    );
 };
 export default Tweet;
